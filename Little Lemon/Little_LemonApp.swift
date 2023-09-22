aaca78c46e88d5283ed512e52372169104872d08
visibilityishidden <126164471+visibilityishidden@users.noreply.github.com>
2023-09-22 17:15:10 +0300

    adding Little Lemon from scratch

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Yaroslav Liashevych on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
